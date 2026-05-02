a2ffda94dd87d643d6415562d4a9eff8b71b9d26
Ashwith Rangu <ranguashwith@gmail.com>
2026-05-02 21:29:15 +0530

    Add src/main/java/com/example/library/model/Book.java

---
+++ src/main/java/com/example/library/model/Book.java
@@ -0,0 +1,37 @@
+package com.example.library.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "ISBN is required")
+    private String isbn;
+
+    @NotNull(message = "Author is required")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public Book() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getIsbn() { return isbn; }
+    public void setIsbn(String isbn) { this.isbn = isbn; }
+
+    public Author getAuthor() { return author; }
+    public void setAuthor(Author author) { this.author = author; }
+}
